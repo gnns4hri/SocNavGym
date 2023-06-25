30e66ac4092bb798600f677b5ed68c657d401825
Sushant S <sushantswamy1212@gmail.com>
2023-06-25 13:29:46 +0530

    add init files to sngnnv2

---
+++ socnavgym/envs/utils/sngnnv2/__init__.py
@@ -0,0 +1,7 @@
+from .socnav import SocNavDataset
+from .socnav_V2_API import Human as otherHuman
+from .socnav_V2_API import Object as otherObject
+from .socnav_V2_API import SNScenario, SocNavAPI
+from .nets.gat import *
+from .nets.mpnn_dgl import *
+from .nets.rgcnDGL import *
